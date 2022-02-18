7ce7db3fc472e450a4e96630a992612c55fb8dc6
Anass <anass.maar@gmail.com>
2022-02-18 09:53:44 +0100

    Update insert.php

--- Prototypes/Prototype 0/insert.php
+++ Prototypes/Prototype 0/insert.php
@@ -16,3 +16,43 @@
 
     }
 ?>
+<div>
+        <div>
+		<div><h3>Create a User</h3>
+        <form method="POST" action="">
+			<div>
+				<label for="inputFName">First Name</label>
+				<input type="text" required="required" id="inputFName" name="fname" placeholder="First Name">
+				<span></span>
+			</div>
+			
+			<div>
+				<label for="inputLName">Last Name</label>
+				<input type="text" required="required" id="inputLName" name="lname" placeholder="Last Name">
+        		<span></span>
+			</div>
+			
+			<div>
+				<label for="inputAge">Age</label>
+				<input type="number" required="required" class="form-control" id="inputAge" name="age" placeholder="Age">
+				<span></span>
+			</div>
+				<div class="form-group">
+					<label for="inputGender">Gender</label>
+					<select class="form-control" required="required" id="inputGender" name="gender" >
+						<option>Please Select</option>
+						<option value="Male">Male</option>
+						<option value="Female">Female</option>
+					</select>
+					<span></span>
+        		</div>
+    
+			<div class="form-actions">
+					<button type="submit">Create</button>
+					<a href="index.php">Back</a>
+			</div>
+		</form>
+        </div></div>        
+</div>
+</body>
+</html>
