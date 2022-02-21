5281fa7c5ebee56ab1ddce86e904994d084d4329
Anass <anass.maar@gmail.com>
2022-02-21 09:14:49 +0100

    Create insert.php

---
+++ Prototypes/Prototype2/insert.php
@@ -0,0 +1,33 @@
+
+<body>
+<div>
+        <div>
+		<div><h3>Create a User</h3>
+        <form method="POST" action="">
+			<div>
+				<label for="inputFName">First Name</label>
+				<input type="text" required="required" id="inputFName" name="fname" placeholder="First Name">
+				<span></span>
+			</div>
+			
+			<div>
+				<label for="inputLName">Last Name</label>
+				<input type="text" required="required" id="inputLName" name="lname" placeholder="Last Name">
+        		<span></span>
+			</div>
+			
+			<div>
+				<label for="inputAge">Age</label>
+				<input type="number" required="required" class="form-control" id="inputAge" name="age" placeholder="Age">
+				<span></span>
+			</div>
+    
+			<div class="form-actions">
+					<button type="submit">Create</button>
+                    
+			</div>
+		</form>
+        </div></div>        
+</div>
+</body>
+</html>
