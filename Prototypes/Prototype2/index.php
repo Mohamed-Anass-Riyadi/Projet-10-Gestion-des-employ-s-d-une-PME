c248a456b0db8a1e42519654b01649c4f424d78a
Anass <anass.maar@gmail.com>
2022-02-21 09:20:56 +0100

    Update index.php

---
+++ Prototypes/Prototype2/index.php
@@ -0,0 +1,30 @@
+
+<body>
+    <div>
+        <a href="insert.php">Insert Data</a>
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+                <th>Action</th>
+            </tr>
+
+            <?php
+                    foreach($data as $person){
+            ?>
+
+            <tr>
+                <td><?= $person['firstname']?></td>
+                <td><?= $person['lastname']?></td>
+                <td><?= $person['age']?></td>
+                <td>
+                    <a href="edit.php?id=<?php echo $person['id'] ?>">Edit</a>
+                    <a href="delete.php?id=<?php echo $person['id'] ?>">delete</a>
+                </td>
+            </tr>
+            <?php }?>
+        </table>
+    </div>
+</body>
+</html>
