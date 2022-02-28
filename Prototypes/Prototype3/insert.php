0cf4fa06ccec2a0efe2236105625e6ba29cfdf40
Anass <anass.maar@gmail.com>
2022-02-28 11:59:48 +0100

    First page html

---
+++ Prototypes/Prototype3/insert.php
@@ -0,0 +1,39 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<div>
+        <div>
+		<div><h3>Create a User</h3>
+        <form method="POST" action="">
+			<div>
+				<label for="inputfname">First Name</label>
+				<input type="text" required="required" id="inputfname" name="fname" placeholder="First Name">
+				<span></span>
+			</div>
+			
+			<div>
+				<label for="inputlname">Last Name</label>
+				<input type="text" required="required" id="inputlname" name="lname" placeholder="Last Name">
+        		<span></span>
+			</div>
+			
+			<div>
+				<label for="inputage">Age</label>
+				<input type="number" required="required" class="form-control" id="inputage" name="age" placeholder="Age">
+				<span></span>
+			</div>
+		
+    
+		
+		</form>
+        </div></div>        
+</div>
+</body>
+</html>
