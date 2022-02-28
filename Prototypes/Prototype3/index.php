ee0e2cd14bd315565877b596442471d9dcffaa49
Anass <anass.maar@gmail.com>
2022-02-28 12:03:22 +0100

    up

---
+++ Prototypes/Prototype3/index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <a href="insert.php">Insert Data</a>
+        <table>
+            <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Age</th>
+                <th>Gender</th>
+                <th>Action</th>
+            </tr>
+
+        </table>
+    </div>
+</body>
+</html>
