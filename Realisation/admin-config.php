abc4aadf21d1ca588b70409a203ab449f66d0200
Mohamed Anass Riyadi <anass.maar@gmail.com>
2022-03-09 19:07:41 +0100

    delete

--- Realisation/admin-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   $admin_config = mysqli_connect('localhost', 'Anass', 'aaaaaaaaaa','admin'
-
-   );
-
-  
-?>
--- Realisation/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-   $conn = mysqli_connect('localhost', 'Anass', 'aaaaaaaaaa', 'realisation');
-
-   // check connection
- if(!$conn){
-      echo 'Connection error: ' . mysqli_connect_error(); 
-  }
-?>
